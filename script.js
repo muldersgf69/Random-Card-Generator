a3c6c434f4ffaa3252662e2b036b36a7bd4de9ce
muldersgf69 <109549032+muldersgf69@users.noreply.github.com>
2024-02-21 01:46:00 +0000

    last commit

--- script.js
+++ script.js
@@ -2,32 +2,24 @@ window.onload = function() {
     const suits = ['♠', '♣', '♥', '♦'];
     const randomSuitIndex = Math.floor(Math.random() * suits.length)
     const randomSuit = suits[randomSuitIndex];
-    console.log(randomSuit);
 
     const values = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
     const randomValueIndex = Math.floor(Math.random()* values.length);
     const randomValue = values[randomValueIndex];
-    console.log(randomValue)
-    
-    const topRow = document.querySelector(".top-row");
-    const middleRow = document.querySelector(".middle-row");
-    const bottomRow = document.querySelector (".bottom-row");
-    
-    const topRowElement = document.createElement('div');
-    const middleRowElement = document.createElement('div');
-    const bottomRowElement = document.createElement('div');
-    
-    const topTextNode = document.createTextNode(randomSuit);
-    const middleTextNode = document.createTextNode(randomValue);
-    const bottomTextNode = document.createTextNode(randomSuit);
-    
-    topRowElement.appendChild(topTextNode);
-    topRow.appendChild(topRowElement);
-
-    middleRowElement.appendChild(middleTextNode);
-    middleRow.appendChild(middleRowElement);
-
-    bottomRowElement.appendChild(bottomTextNode);
-    bottomRow.appendChild(bottomRowElement);
-
-}
+     
+    const cardValueElement = document.querySelector(".card-value");
+    const topSuitElement = document.querySelector(".top-suit");
+    const bottomSuitElement = document.querySelector(".bottom-suit");
+     
+   cardValueElement.textContent = randomValue;
+   topSuitElement.textContent = randomSuit;
+   bottomSuitElement.textContent = randomSuit;
+   
+    const colors = ['red','black'];
+    const randomColorIndex = Math.floor(Math.random()* colors.length);
+    const randomColor = colors[randomColorIndex];
+     
+    cardValueElement.style.color = randomColor;
+     topSuitElement.style.color = randomColor;
+     bottomSuitElement.style.color = randomColor;
+};
